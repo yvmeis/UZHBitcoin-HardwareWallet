8678d26da7d93c453df6cd925cc478e78b59065f
robaet <71385291+robaet@users.noreply.github.com>
2021-11-12 16:40:29 +0100

    Update pin.py

--- most secure pin in the world/pin.py
+++ most secure pin in the world/pin.py
@@ -1,3 +1,24 @@
+from typing import ByteString
 import bcrypt
-pw = input("input your password: ")
-print(pw)
+
+#returns the hashed password
+def hashPW(pw):
+    pw = pw.encode()
+    hashed = bcrypt.hashpw(pw, bcrypt.gensalt())
+    print("---hashed---")
+    return hashed
+
+#returns True if the hashed password matches the plain text password
+def checkPW(pw, hashed):
+    pw = pw.encode()
+    if bcrypt.checkpw(pw, hashed):
+        print("strings are equivalent")
+        return True
+    else:
+        print("strings are NOT equivalent")
+        return False
+
+
+
+pw = input("password: ")
+checkPW(pw, hashPW(pw))
