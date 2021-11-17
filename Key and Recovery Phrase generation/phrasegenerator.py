1625c95c8805730984315c37614b67b5ec5c537f
robaet <71385291+robaet@users.noreply.github.com>
2021-11-18 00:18:40 +0100

    lauft nonig

--- Key and Recovery Phrase generation/phrasegenerator.py
+++ Key and Recovery Phrase generation/phrasegenerator.py
@@ -1,4 +1,4 @@
-import os,binascii,bitcoin
+import os, binascii, secrets, hashlib
 
 def generate_entropy():
     hexstring = binascii.b2a_hex(os.urandom(16))
@@ -8,4 +8,48 @@ def generate_entropy():
 def generate_recovery_phrases(hexstring):
     phrases = bip39.mnemonic_from_bytes(hexstring)
 
-generate_recovery_phrases(generate_entropy)
+
+
+################################################################
+
+#length between 128 and 256. length has to be divisible by 32
+def gen_entropy(length):
+    entropy = os.urandom(length//8)
+    print(entropy)
+    return (entropy)
+
+def hash_entropy(entropy):
+    h = hashlib.sha256(entropy).hexdigest()
+    print()
+    print(h)
+    b = (
+            bin(int.from_bytes(entropy, byteorder="big"))[2:].zfill(len(entropy) * 8)
+            + bin(int(h, 16))[2:].zfill(256)[: len(entropy) * 8 // 32]
+        )
+    print()
+    print(str(b))
+    return str(b)
+    
+    
+    
+    
+    #x = hashlib.sha256()
+    #x.update(entropy)
+    #hashed_entropy = x.digest()
+    
+    #return hashed_entropy
+    
+def find_words(binary):
+    file = open('english.txt', 'r')
+    words = []
+    phrase = ""
+    for x in file:
+        words.append(x)
+    file.close()
+    for x in range(0, len(binary), 11):
+        print(int(binary[x:x+11], 2))
+        phrase = phrase + words[int(binary[x:x+11], 2)] + " "
+    print(phrase)
+
+a = hash_entropy(gen_entropy(128))
+find_words(a)
