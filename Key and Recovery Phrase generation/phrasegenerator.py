23ddc42bcc35e86a3b6cbeefcaeefe720a5fa6ef
Yves-Meister <yves.meister@uzh.ch>
2021-11-18 14:50:21 +0100

    Update phrasegenerator.py

--- Key and Recovery Phrase generation/phrasegenerator.py
+++ Key and Recovery Phrase generation/phrasegenerator.py
@@ -24,9 +24,9 @@ def find_words(binary):
         phrase.append(words[int(binary[x:x+11], 2)].rstrip())
     print(phrase)
 
-def gen_seed(mnemonic_bytes, passphrase = ''):
+def gen_seed(mnemonic_bytes, passphrase = 'TREZOR'):
     # The passphrase is a phrase the user should be able to choose in order to make things more secure!
-    password = mnemonic_bytes.decode('utf-8')
+    password = mnemonic_bytes.hex()
     password = unicodedata.normalize('NFKD', password)
     salt = 'mnemonic' + passphrase
     salt = unicodedata.normalize('NFKD', salt)
@@ -44,6 +44,6 @@ find_words(hash_entropy(gen_entropy(128), 128))
 #########################################tests##################################################
 ################################################################################################
 
-ma_seed = gen_seed(b'c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04')
+ma_seed = gen_seed(bytes.fromhex('c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04'))
 #ma_seed.decode('utf-8')
-print (ma_seed)
+print (ma_seed.hex())
