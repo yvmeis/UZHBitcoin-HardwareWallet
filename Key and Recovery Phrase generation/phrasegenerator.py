2e6c29e71615f80da7616431ab3f369e879131c4
robaet <71385291+robaet@users.noreply.github.com>
2021-11-18 13:25:45 +0100

    Update phrasegenerator.py

--- Key and Recovery Phrase generation/phrasegenerator.py
+++ Key and Recovery Phrase generation/phrasegenerator.py
@@ -1,16 +1,18 @@
-import os, binascii, secrets, hashlib, sys
+import os, hashlib, sys
 
 
 #length between 128 and 256. length has to be divisible by 32
 def gen_entropy(length):
     entropy = os.urandom(length//8)
-    return (entropy)
+    return entropy
 
 def hash_entropy(entropy, length):
     h = hashlib.sha256(entropy).hexdigest()
     b = bin(int(h, 16))[2:].zfill(256)
     checksum = b[0:int(length/32)]
+    print(checksum)
     entropy_bytes = bin(int.from_bytes(entropy, byteorder = sys.byteorder))[2:].zfill(length)
+    print(entropy_bytes)
     mnemonic_bytes = entropy_bytes + checksum
     return mnemonic_bytes
     
@@ -23,6 +25,7 @@ def find_words(binary):
     file.close()
     for x in range(0, len(binary), 11):
         phrase.append(words[int(binary[x:x+11], 2)].rstrip())
+        print(int(binary[x:x+11],2))
     print(phrase)
 
 
