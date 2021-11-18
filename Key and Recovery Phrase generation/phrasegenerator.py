f1b649a2dc541a6b5f02649be45752db5e6ba87b
Yves-Meister <yves.meister@uzh.ch>
2021-11-18 14:06:04 +0100

    bug fixes

--- Key and Recovery Phrase generation/phrasegenerator.py
+++ Key and Recovery Phrase generation/phrasegenerator.py
@@ -1,5 +1,5 @@
-import os, binascii, secrets, hashlib, sys
-
+import os, binascii, secrets, hashlib, sys, unicodedata
+from typing import AnyStr
 
 #length between 128 and 256. length has to be divisible by 32
 def gen_entropy(length):
@@ -25,13 +25,18 @@ def find_words(binary):
         phrase.append(words[int(binary[x:x+11], 2)].rstrip())
     print(phrase)
 
-def gen_seed(mnemonic_bytes, passphrase = b''):
+def gen_seed(mnemonic_bytes, passphrase = ''):
     # The passphrase is a phrase the user should be able to choose in order to make things more secure!
-    password = mnemonic_bytes
-    salt = b'mnemonic' + passphrase
+    password = mnemonic_bytes.decode('utf-8')
+    password = unicodedata.normalize('NFKD', password)
+    salt = 'mnemonic' + passphrase
+    salt = unicodedata.normalize('NFKD', salt)
     iterations = 2048
+    salt = salt.encode('utf-8')
+    password = password.encode('utf-8')
     seed = hashlib.pbkdf2_hmac('sha512',password,salt,iterations)
     return seed
+
 
 find_words(hash_entropy(gen_entropy(128), 128))
 
@@ -41,4 +46,5 @@ find_words(hash_entropy(gen_entropy(128), 128))
 ################################################################################################
 
 ma_seed = gen_seed(b'c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04')
+ma_seed.decode('utf-8')
 print (ma_seed)
